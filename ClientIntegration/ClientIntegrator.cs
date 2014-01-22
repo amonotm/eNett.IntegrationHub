4cae6ebbd151e0a9b87828269bbfbce49c596d7d
amonotm <adam.mills@rxpservices.com>
2014-01-22 18:30:59 +1100

    Added ClientIntegration Project

---
+++ ClientIntegration/ClientIntegrator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eNett.IntegrationHub.BusinessObjects;
+using eNett.IntegrationHub.SharedInterfaces;
+
+namespace ClientIntegration
+{
+    public class ClientIntegrator
+    {
+        public void PostChanges(IClientRepository clientRepository)
+        {
+            
+        }
+
+        private List<Change> GetClientChanges()
+        {
+            
+        }
+
+        private List<Change> GetClientContactChanges()
+        {
+
+        }
+    }
+}
